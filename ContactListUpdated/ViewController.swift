d5cc372f0f7123b4cbba749251a0e192a8dc0635
Avb2 <bringuel.alexander@gmail.com>
2025-04-07 23:08:29 -0400

    Initial Commit

---
+++ ContactListUpdated/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListUpdated
+//
+//  Created by Alex Bringuel on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
